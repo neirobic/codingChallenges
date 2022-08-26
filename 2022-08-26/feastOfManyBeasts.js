2c036fc6a91173e29a8f2fb82a0a1a568a7e8a36
Victor Neiro <neirobic@gmail.com>
2022-08-26 16:26:02 -0400

    add daily coding challenge for 2022-08-26

---
+++ 2022-08-26/feastOfManyBeasts.js
@@ -0,0 +1,5 @@
+function feast(beast, dish) {
+    //check if first letter of beast and dish are the same
+      return (beast[0]===dish[0] && beast[beast.length-1]===dish[dish.length-1]);
+      
+    }
