7114515480da83fd67f97fe180a60675fd4ed8df
Victor Neiro <neirobic@gmail.com>
2022-09-08 16:39:26 -0400

    added daily coding challenge for 2022-09-08

---
+++ 2022-09-08/clock.js
@@ -0,0 +1,8 @@
+function past(h, m, s){
+    //#Happy Coding! ^_^
+    //1 second = 1000 miliseconds
+    //1 minute = 60*(1000 miliseconds)
+    //1 hour = 60*60*1000 miliseconds
+    //right?
+    return(h*60*60*1000+m*60*1000+s*1000)
+  }
