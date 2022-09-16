bf8feeb4c6eca41ba298c6f4dd6dcd51854a0cbc
Victor Neiro <neirobic@gmail.com>
2022-09-16 16:43:54 -0400

    added daily coding challenge for 2022-09-16

---
+++ 2022-09-16/evenOrOdd.js
@@ -0,0 +1,6 @@
+function even_or_odd(number) {
+    // (number) => string
+    // take integer as an argument, return 'Even' for even numbers
+    // and 'Odd' for odd numbers;
+    return (number % 2 == 0 ? 'Even' : 'Odd');  
+  }
