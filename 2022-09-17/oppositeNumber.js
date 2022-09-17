47626e5c810cf312d12712a004f17ed15d6a4d83
Victor Neiro <neirobic@gmail.com>
2022-09-17 16:44:31 -0400

    added daily coding challenge for 2022-09-17

---
+++ 2022-09-17/oppositeNumber.js
@@ -0,0 +1,9 @@
+function opposite(number) {
+    //(number) => number
+    //given an integer or floating-point number, find the opposite number
+    // (1) => -1
+    // (14) => -14
+    // (-34) => 34
+    
+    return number * -1;
+  }
