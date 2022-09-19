e1e24669fad1b6f1bba7f076376c1bf8c8ba420f
Victor Neiro <neirobic@gmail.com>
2022-09-19 16:45:29 -0400

    added daily coding challenge for 2022-09-19

---
+++ 2022-09-19/returnNegative.js
@@ -0,0 +1,6 @@
+function makeNegative(num) {
+    // (number) => number
+    // if number < 0, return number. if number > 0, 
+    //convert number to negative and return that
+    return ((num > 0) ? num * -1 : num) ; 
+  }
