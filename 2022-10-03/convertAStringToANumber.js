4b1f70d3bb5b765cc4b88a57a3d226e3d69886dd
Victor Neiro <neirobic@gmail.com>
2022-10-03 17:05:00 -0400

    added daily coding challenge for 2022-10-03

---
+++ 2022-10-03/convertAStringToANumber.js
@@ -0,0 +1,10 @@
+const stringToNumber = function(str){
+    /* (string) => number
+      convert string to a number
+      "1234" --> 1234
+      "605"  --> 605
+      "1405" --> 1405
+      "-7" --> -7
+    */
+    return Number(str);
+  }
