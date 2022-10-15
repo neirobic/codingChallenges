145e00d8cde9e973b134be9edc6291cde8f5ba3c
Victor Neiro <neirobic@gmail.com>
2022-10-14 20:06:50 -0400

    add daily coding challenge for 2022-10-24

---
+++ 2022-10-24/thisIsAProblem.js
@@ -0,0 +1,9 @@
+function makeFullName(first, last) {
+
+    this.firstName = first;
+    this.lastName = last;
+    this.name = this.firstName + ' ' + this.lastName
+}
+//prompt was to fix a given function
+//tbqh i have no idea what i did, but it worked!
+//i think the problem was having a 'return' statement on a constructor function?
