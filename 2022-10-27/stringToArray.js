8c50e5360eee5822dcd987e5a51c8faf4bced23a
Victor Neiro <neirobic@gmail.com>
2022-10-27 21:04:28 -0400

    add daily coding challenge for 2022-10-27

---
+++ 2022-10-27/stringToArray.js
@@ -0,0 +1,8 @@
+function stringToArray(string){
+    /* (string) => array
+    return an array with the contents of string as its elements, split at ' '.
+    */
+      
+      return string.split(' ');
+    
+    }
