86a7e0d54b6ead9b0eda40670bed1afb11194cbe
Victor Neiro <neirobic@gmail.com>
2022-11-15 10:05:10 -0500

    add daily coding challenge for 2022-11-15

---
+++ 2022-11-15/volumeOfACuboid.js
@@ -0,0 +1,12 @@
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    /* (number, number, number) => number
+      returns length * width * height
+      
+    getVolumeOfCuboid(1,1,1) => 1
+    getVolumeOfCuboid(2,3,5) => 30
+    getVolumeOfCuboid(236.56,1204.234,0) => 0
+    */
+    return length*width*height;
+  }
+}
